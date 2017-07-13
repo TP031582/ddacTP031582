eee0cf55e6b40634a1eec590d5a562c068e61f49
TP031582 <jack.1995@hotmail.com>
2017-07-13 22:22:08 +0800

    CMS
    CMS

---
+++ WebApplication1/WebApplication1/Models/addScheduleViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [System.Runtime.Serialization.DataContract(IsReference = true)]
+    [System.ComponentModel.DataAnnotations.ScaffoldTable(true)]
+    public class addScheduleViewModel
+    {
+        
+        [Display(Name = "Departure")]
+        [Required]
+        public string departure { get; set; }
+        
+        [Display(Name = "Destination")]
+        [Required]
+        public string destination { get; set; }
+        
+        [Display(Name = "Weight")]
+        [Required]
+        public string weight { get; set; }
+  
+        [Display(Name = "Deliver Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Required]
+        public System.DateTime deliver_date { get; set; }
+    }
+}
